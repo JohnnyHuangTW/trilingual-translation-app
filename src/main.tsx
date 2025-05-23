d0172e8d356ca242184c090cfb8926dceec1c05d
johnnyhuangtw <jtk155586@gmail.com>
2025-05-23 14:22:53 +0200

    Add react-scan for performance monitoring and update package dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { scan } from 'react-scan'
+import App from './App.tsx'
+import './index.css'
 
-import { TempoDevtools } from "tempo-devtools";
-TempoDevtools.init();
+import { TempoDevtools } from 'tempo-devtools'
+TempoDevtools.init()
 
-const basename = import.meta.env.BASE_URL;
+const basename = import.meta.env.BASE_URL
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter basename={basename}>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-);
+scan({
+	enabled: process.env.NODE_ENV === 'development',
+})
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+	<React.StrictMode>
+		<BrowserRouter basename={basename}>
+			<App />
+		</BrowserRouter>
+	</React.StrictMode>,
+)
